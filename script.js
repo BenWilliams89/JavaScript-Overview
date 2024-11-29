442ba6fae3fee22313b15fe99d9352fcc2e6e7cb
Ben Williams <codebencode@icloud.com>
2024-11-29 15:41:03 +0000

    Add javaScript basics

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert ("Welcome")
+document.write('Hello');
+
+// variables
+let name = 'Ben'
+const lastName = "Williams"
+console.log(name)
+console.log(name, lastName)
+
+// Scope - visability - 3 types - Global, function & block
+
+// Data Types - Primitive & Reference
+    //primitive = String, Number, Boolean, Null, Undefined, BigInt, Symbol
+    //Reference = Object, Array, Function
