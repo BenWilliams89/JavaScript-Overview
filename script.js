a92c45dc1546311be60abd201d4926ff59595371
Ben Williams <codebencode@icloud.com>
2024-11-29 15:58:51 +0000

    Add variables and Data Types

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ console.log(name, lastName)
 // Data Types - Primitive & Reference
     //primitive = String, Number, Boolean, Null, Undefined, BigInt, Symbol
     //Reference = Object, Array, Function
+
+    //object - collection of properties - key:value pair
+    let person = {
+        name: 'Ben',
+        age: 35,
+    }
+    console.log(person)
+
+    //Array - stores a collection of values
+
+    let number = [1,2,3,4,5];
+
+    console.log(number);
+    console.log(typeof number);
+
+    //Function
+    function msg(){
+        console.log('Hello, Ben')
+    }
+
+    console.log(typeof msg);
+    console.log(msg);
+    msg()
+
+    //Operaters = + - * / = attrithmatic (Maths)
+    let sum = 5 + 3;
+    console.log(sum);
